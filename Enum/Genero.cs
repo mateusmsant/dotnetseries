f65dbb1ea49b0b88be2a86af0a9faa0fdec2aa71
Mateus Santana <mateusmsant@gmail.com>
2021-06-19 09:12:35 -0300

    first commit

---
+++ Enum/Genero.cs
@@ -0,0 +1,19 @@
+namespace series
+{
+    public enum Genre
+    {
+        Action = 1,
+        Adventure = 2,
+        Comedy = 3,
+        Documentary = 4,
+        Drama = 5,
+        Espionage = 6,
+        Western = 7,
+        Fantasy = 8,
+        Science_Fiction = 9,
+        Musical = 10,
+        Romance = 12,
+        Thriller = 13,
+        Terror = 14,
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Serie serie = new Serie();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
